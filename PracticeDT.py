27156414f6c6d4d5f212cdf9c4d1c06545874de7
OlegKo2024 <4oki66@gmail.com>
2024-06-20 19:18:06 +0300

    Add files via upload

---
+++ PracticeDT.py
@@ -0,0 +1,9 @@
+print('Homework3')
+name ='Oleg'
+print('Name:',name) # Cоздайте переменную name и присвойте ей значение вашего имени (строка)
+age = 57 # Создайте переменную age и присвойте ей значение вашего возраста
+print('Age:' ,age) # Выведите значение переменной age на экран
+age = 57 + 3 # Перезапишите в age текущее значение переменной age + новое
+print('New age:', age)
+is_student = True
+print('Is student:', is_student)
